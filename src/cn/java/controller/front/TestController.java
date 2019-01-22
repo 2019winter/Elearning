bd7d8fe348412e80e9a67158bb74e7b7b1d8a77b
Chen Yanyu <530379451@qq.com>
2019-01-22 23:24:00 +0800

    删除applicationContext.xml错误配置+注释mapper某一错误接口

--- src/cn/java/controller/front/TestController.java
+++ src/cn/java/controller/front/TestController.java
@@ -1,5 +1,6 @@
 package cn.java.controller.front;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +23,8 @@ public class TestController {
     // 测试错误处理
     // 测试通过
   }
+
+
 
 
 
